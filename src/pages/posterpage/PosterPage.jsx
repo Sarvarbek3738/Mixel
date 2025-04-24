d4e51195725a15b2c05849ea5232640349e8ed7a
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-04-24 18:02:51 +0500

    qwertyuio

--- src/pages/posterpage/PosterPage.jsx
+++ src/pages/posterpage/PosterPage.jsx
@@ -3,7 +3,15 @@ import "./PosterPage.css";
 import ProductBox from "../../components/productBox/ProductBox";
 import { useParams } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
-function PosterPage() {
+function PosterPage({getUser,
+  userData,
+  key,
+  deleteFromLiked,
+  addToLiked,
+  getOneProductData,
+  setShowOrderModal,
+  item,
+  getData}) {
   const id = useParams();
   const [banner, setBanner] = React.useState([]);
   const [posterProducts, setPosterProducts] = React.useState([]);
@@ -50,39 +58,108 @@ function PosterPage() {
   return (
     <div className="posterPage">
       <div className="container">
-      <div className="basicTitle">
-            <div className="basicTitleLeft">
+        <div className="basicTitle">
+          <div className="basicTitleLeft">
+            <div>
+              <p>Home</p>
               <div>
-                <p>Home</p>
-                <div>
-                  <i class="fa-solid fa-chevron-right"></i>
-                </div>
-              </div>
-              <div>
-                <p>PosterPage</p>
+                <i class="fa-solid fa-chevron-right"></i>
               </div>
             </div>
-           
+            <div>
+              <p>PosterPage</p>
+            </div>
           </div>
-        {banner?.results
-          ?.filter((item) => item.id == id.id)
-          .map((item) => (
-            <div className="posterBanner" key={item.id}>
-              <img src={item.image} alt="" />
-            </div>
-          ))}{" "}
-          {!banner && (
-            <Skeleton
-              variant="rectangular"
-              style={{ marginTop: "60px", marginBottom: "10px" }}
-              width={1300}
-              height={324}
-            />
-          )}
+
+        </div>
+        {!banner?.results?.length ? (
+          <Skeleton
+            variant="rectangular"
+            style={{ marginBottom: "10px" }}
+            width={1300}
+            height={324}
+          />
+        ) : (
+          banner.results
+            .filter((item) => item.id == id.id)
+            .map((item) => (
+              <div className="posterBanner" key={item.id}>
+                <img src={item.image} alt="Banner" />
+              </div>
+            ))
+        )}
         <div className="posterProducts">
-          {posterProducts?.results?.map((item) => {
+          {/* {posterProducts?.results?.map((item) => {
             return <ProductBox item={item} />;
-          })}
+          })} */}
+          {posterProducts?.results?.map((item , index ) => {
+            if (index < 10) {
+              return (
+                <ProductBox
+                  getUser={getUser}
+                  userData={userData}
+                  key={item.id}
+                  deleteFromLiked={deleteFromLiked}
+                  addToLiked={addToLiked}
+                  getOneProductData={getOneProductData}
+                  setShowOrderModal={setShowOrderModal}
+                  item={item}
+                  getData={getData}
+                />
+              );
+            } else {
+              return;
+            }
+          }) ||
+            [1, 2, 3, 4, 5, 1, 2, 3, 4, 5].map((item) => {
+              return (
+                <div className="loadingSkeletons">
+                  <Skeleton
+                    variant="rectangular"
+                    width={230}
+                    height={210}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    style={{ marginTop: "30px" }}
+                    width={230}
+                    height={18}
+                  />
+                  <Skeleton
+                    variant="rectangular"
+                    style={{ marginTop: "20px" }}
+                    width={230}
+                    height={32}
+                  />
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                    }}
+                    className="skeletonButtons"
+                  >
+                    <Skeleton
+                      variant="rectangular"
+                      style={{ marginTop: "20px" }}
+                      width={50}
+                      height={42}
+                    />
+                    <Skeleton
+                      variant="rectangular"
+                      style={{ marginTop: "20px" }}
+                      width={50}
+                      height={42}
+                    />
+                    <Skeleton
+                      variant="rectangular"
+                      style={{ marginTop: "20px" }}
+                      width={50}
+                      height={42}
+                    />
+                  </div>
+                </div>
+              );
+            })}
         </div>
       </div>
     </div>
